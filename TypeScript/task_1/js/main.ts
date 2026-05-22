47b071e9f84c94c83265c2c9a7f7910088f16657
benji3106 <benjamin3106@live.fr>
2026-05-22 10:28:06 +0200

    feat: Implement Directors interface, printTeacher function, and StudentClass with methods

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+
 // ==========================================
 // TASK 1 : Teacher Interface & Instance
 // ==========================================
@@ -19,4 +20,71 @@ const teacher3: Teacher = {
   contract: false,
 };
 
+// ==========================================
+// TASK 2 : Directors Interface (Inheritance)
+// ==========================================
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Exemple
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+// ==========================================
+// TASK 3 : printTeacher Function
+// ==========================================
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+// ==========================================
+// TASK 4 : StudentClass
+// ==========================================
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+const student = new StudentClass('John', 'Doe');
+// ==========================================
+// EXECUTIONS / CONSOLE LOGS
+// ==========================================
 console.log(teacher3);
+console.log(director1);
+console.log(printTeacher("John", "Doe"));
+console.log(student.displayName());
+console.log(student.workOnHomework());
+// Exports
